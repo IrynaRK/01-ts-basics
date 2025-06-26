cd5b7e08e09a4ca0a06907b6af4acfc7ed3101a4
IrynaRK <105289016+IrynaRK@users.noreply.github.com>
2025-06-26 21:09:52 +0200

    hw1 fix

--- src/task-3.ts
+++ src/task-3.ts
@@ -2,7 +2,10 @@ const usernames: string[] = ["alice", "bob", "charlie"];
 
 const ratings: number[] = [4.5, 3.8, 5];
 
-interface Product {readonly id: number; title: string;};
+interface Product {
+  readonly id: number; 
+  title: string;
+};
 
 const products: Product[] = [
   { id: 1, title: "Phone" },
